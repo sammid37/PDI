dddef7988d97f7ff69a47c838a1108135d021ee4
sammid37 <samanthadmedeiros37@gmail.com>
2023-11-01 16:58:17 -0300

    📜 wip shuffle data script

--- Identificador_PANCs/shuffle_data.py
+++ Identificador_PANCs/shuffle_data.py
@@ -2,6 +2,23 @@ import os
 import random
 import shutil
 
+'''
+todo:
+* Permitir copiar os arquivos .txt das imagens que foram copiadas (lembre que os arquivos .txt tem o nome da classe e o id da foto da classe igual ao nome da foto)
+* Fazer uma função que armazena em um arquivo .txt ou .json a quantidade de dados de treinamento, testes e validação de cada classe 
+  Exemplo:
+  calendula: {
+    treinamento: 325,
+    testes: 154,
+    validacao: 40,
+  },
+  chicoria: {
+    treinamento: 325,
+    testes: 154,
+    validacao: 40,
+  },
+'''
+
 # Diretório raiz do seu dataset
 dataset_root = '/Users/saman/OneDrive/Documentos/UFPB/PDI/Identificador_PANCs/dataset/'
 
@@ -11,9 +28,8 @@ val_dir = os.path.join(dataset_root, 'validation')
 test_dir = os.path.join(dataset_root, 'test')
 
 # Taxas de divisão
-train_ratio = 0.7
-val_ratio = 0.15
-test_ratio = 0.15
+train_ratio = 0.9
+test_ratio = 0.10
 
 # Lista de classes (nomes dos diretórios de imagem)
 classes = os.listdir(os.path.join(dataset_root, 'images'))
@@ -35,10 +51,14 @@ for cls in classes:
 
   # Calcular os tamanhos de cada conjunto
   total_files = len(img_files)
+
   print(f"Total de arquivos em {cls}: {total_files}")
   num_train = int(total_files * train_ratio)
+  # Calcula a taxa de validação 
+  # 10% dos 90% de cada classe
+  val_ratio = int(0.1 * num_train)  
   num_val = int(total_files * val_ratio)
-  num_test = total_files - num_train - num_val
+  num_test = int(total_files - num_train - num_val)
   print(num_train, num_val, num_test)
 
   # Dividir os arquivos em conjuntos de treinamento, validação e teste
@@ -51,7 +71,9 @@ for cls in classes:
   os.makedirs(os.path.join(val_dir, cls), exist_ok=True)
   os.makedirs(os.path.join(test_dir, cls), exist_ok=True)
 
-  # Copiar arquivos de imagem para os diretórios apropriados
+  # Cria um arquivo .txt com as quantidade de 
+
+  # Copia os arquivos de imagem para os diretórios apropriados
   for img_file in train_files:
     shutil.copy(os.path.join(img_class_dir, img_file), os.path.join(train_dir, cls, img_file))
   for img_file in val_files:
